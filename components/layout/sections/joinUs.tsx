f7e46a0e7bdb5fba483619803c7562241c3fea15
paul <dongpoding@126.com>
2024-11-30 20:07:05 +0800

    fix: types error fix

--- components/layout/sections/joinUs.tsx
+++ components/layout/sections/joinUs.tsx
@@ -5,23 +5,36 @@ import { ArrowRight } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
-import localFont from 'next/font/local'
+import localFont from "next/font/local"
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
  
 // Font files can be colocated inside of `pages`
 const myFont = localFont({ src: [{
-  path: './fonts/Hupaisa-Bold.otf',
-  weight: '400',
-  style: 'normal',
+  path: "./fonts/Hupaisa-Bold.otf",
+  weight: "400",
+  style: "normal",
 }] })
+
+
+const BgBorder = () => {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+  const { theme } = useTheme();
+  return theme === "light" && isLoaded? (
+    <img src="/bg_top_border.png" alt="grid-item-bg1" className="absolute top-0 left-0 w-full h-4 z-[1] object-cover" />
+  ): null
+}
 
 export const JoinUsSection = () => {
   const { theme } = useTheme();
   return (
-    <section id="Franchising and Events" className={cn("container w-full px-16 relative", `bg-[${theme === 'light' ? '#FFF1F8' : '#000'}]`)}>
-      {
-        theme === "light"? <img src={theme === "light" ? "/bg_top_border.png" : "/bg_top_border.png"} alt="grid-item-bg1" className="absolute top-0 left-0 w-full h-4 z-[1] object-cover" />: null
-      }
+    <section id="Franchising and Events" className={cn("container w-full px-16 relative bg-[#FFF1F8]")}>
+      
+      <BgBorder />
       
       <div className="flex xl:justify-between flex-col xl:flex-row lg:flex-row align-center items-center gap-8 mx-auto py-28 md:py-28">
         <div>
@@ -29,7 +42,7 @@ export const JoinUsSection = () => {
                 <p>Open opportunities</p>
                 <p>to join!</p>
             </div>
-            <p className="mt-6 mb-8 text-lg">At Soul Smoothie, we're always on the lookout for passionate individuals who share our commitment to health and wellness. Join our team and be part of a vibrant community dedicated to making every bite and sip a soulful experience. Whether you're a smoothie aficionado or a culinary enthusiast, there’s a place for you here. Explore exciting opportunities to grow with us!</p>
+            <p className="mt-6 mb-8 text-lg">At Soul Smoothie, we&apos;re always on the lookout for passionate individuals who share our commitment to health and wellness. Join our team and be part of a vibrant community dedicated to making every bite and sip a soulful experience. Whether you&ldquo;re a smoothie aficionado or a culinary enthusiast, there&lsquo;s a place for you here. Explore exciting opportunities to grow with us!</p>
             <div className="flex gap-8 mb-8">
                 <div className="flex flex-col">
                     <h3 className={cn("text-primary text-2xl font-bold uppercase", myFont.className)}>Subheading one</h3>
